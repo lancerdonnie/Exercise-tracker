1263791f352055747c7f67838c58e2eb62b331d0
lancerdonnie <jideadedejifirst@gmail.com>
2020-02-04 13:25:39 +0100

    deployment

--- server.js
+++ server.js
@@ -14,10 +14,11 @@ ConnectDB();
 if(process.env.NODE_ENV==="production"){
   app.use(express.static("client/build"))
   app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"client","build","index.html"))
+    res.sendFile(path.join(__dirname,"client","build","index.html"))
   })
 }
 
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
   console.log('dev server running on port 5000');
 });
