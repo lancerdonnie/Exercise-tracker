857c273fcfe662695219a85dafdbf3b757a15b62
lancerdonnie <jideadedejifirst@gmail.com>
2020-02-04 12:55:11 +0100

    deployment

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const path=require("path")
+
 app.use(express.json());
 
 app.use('/users', require('./routes/users'));
@@ -7,6 +9,14 @@ app.use('/exercises', require('./routes/exercises'));
 
 const ConnectDB = require('./config/db');
 ConnectDB();
+
+
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static("client/build"))
+  app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"client","build","index.html"))
+  })
+}
 
 app.listen(5000, () => {
   console.log('dev server running on port 5000');
